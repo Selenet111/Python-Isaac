5fe206b1b0edb613c8adeabec9e4d9ce8ad83c3e
Selenet111 <isaac.chan@rogers.com>
2026-03-07 20:29:02 -0500

    Update strings.py

--- strings.py
+++ strings.py
@@ -33,15 +33,15 @@
 
 # print(listOfFixedNames)
 
-#____________________________EXAMPLE___________________________________
+#______________________________________TASK 2_______________________________
 
 highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
-allthepoems = []
+finalpoems = []
 
 splitpoems = highlighted_poems.split(", ")
 
 for poem in splitpoems:
     moresplit = poem.split(":")
-    allthepoems.append(moresplit)
+    finalpoems.append(moresplit)
 
-print(allthepoems)
+print(finalpoems)
